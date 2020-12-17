0e8bebdd78fea2592a5caeb67ccaaff28fbfaa5c
Friedrich_Cheng94 <48461667+Friedrich94326@users.noreply.github.com>
2020-12-18 06:50:13 +0800

    Add files via upload

---
+++ new_calculate_face_embeddings.py
@@ -0,0 +1,27 @@
+# Packages for creating face embeddings
+from numpy import load
+from numpy import expand_dims
+from numpy import asarray
+from numpy import savez_compressed
+from keras.models import load_model
+
+# get the face embedding for one face based on a specified deep network model
+
+def get_embedding(model, face_pixels):
+    
+    face_pixels = face_pixels.astype('float32') # scale pixel values
+
+    
+    # pre-whitening process: pixel values across channels (globally)
+    mean, std = face_pixels.mean(), face_pixels.std() 
+    face_pixels = (face_pixels - mean) / std
+    
+    # (OPTIONAL) L2-normalisation: enhance facial characteristics
+    
+    samples = expand_dims(face_pixels, axis = 0)
+    
+    
+    yhat = model.predict(samples)
+    
+    return yhat[0]
+
