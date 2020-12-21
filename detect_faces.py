da4e955bd2588944d196fa76904821886e0eb959
Friedrich_Cheng94 <48461667+Friedrich94326@users.noreply.github.com>
2020-12-21 08:49:31 +0800

    Update detect_faces.py

--- detect_faces.py
+++ detect_faces.py
@@ -1,4 +1,4 @@
-# Import packages for face detection
+# Libraries/ Packages for face detection
 from os import listdir
 from os.path import isdir
 from PIL import Image
@@ -21,14 +21,14 @@ def MTCNN_detect_face(filename, required_size = (160, 160, 3)):
     print('[INFO] Loading image file: ', filename) # for bebugging
     
     img = cv2.imread(filename) # BGR image
-    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # converted to RGB format
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # Converted to RGB format
     
     detector = MTCNN()
     results = detector.detect_faces(img)
    
-    n_faces = len(results) # how many faces got detected
+    n_faces = len(results) # How many faces got detected
         
-    # extract a face (the 1st face detected)
+    # Extract a face (the 1st face detected)
     if n_faces > 0:
         x1, y1, width, height = results[0]['box']
         x1, y1 = abs(x1), abs(y1)
@@ -36,7 +36,7 @@ def MTCNN_detect_face(filename, required_size = (160, 160, 3)):
 
         face = img_rgb[y1:y2, x1:x2, :]
 
-        # rsize pixels to the required size for FaceNet model: (160, 160, 3)
+        # Resize pixels to the required size for FaceNet model: (160, 160, 3)
         face_image = Image.fromarray(face)  # face_image: PIL.Image.Image
         image = np.resize(face_image, required_size)  # image: ndarray
         face_array = asarray(image)  # face_array: ndarray
@@ -56,11 +56,10 @@ def MTCNN_detect_face(filename, required_size = (160, 160, 3)):
         no_face_array = np.zeros(shape = (160, 160, 3))
         return no_face_array
 
+    
+    
+    
 """ Detect faces using PIL.Image.open() """
-
-
-
-
 
 # Detect a single face from a given photo
 def MTCNN_detect_face_2(filename, required_size = (160, 160)):
@@ -68,15 +67,15 @@ def MTCNN_detect_face_2(filename, required_size = (160, 160)):
     print('[INFO] Loading image file: ', filename) # for bebugging
     
     img = Image.open(filename) # BGR image
-    img = img.convert('RGB') # converted to RGB format
+    img = img.convert('RGB') # Converted to RGB format
     pixels = asarray(img)
     
     detector = MTCNN()
     results = detector.detect_faces(pixels)
    
-    n_faces = len(results) # how many faces got detected
+    n_faces = len(results) # How many faces got detected
         
-    # extract a face (the 1st face detected)
+    # Extract a face (the 1st face detected)
     if n_faces > 0:
         x1, y1, width, height = results[0]['box']
         x1, y1 = abs(x1), abs(y1)
@@ -84,7 +83,7 @@ def MTCNN_detect_face_2(filename, required_size = (160, 160)):
 
         face = pixels[y1:y2, x1:x2]
 
-        # rsize pixels to the required size for FaceNet model: (160, 160, 3)
+        # Resize pixels to the required size for FaceNet model: (160, 160, 3)
         img = Image.fromarray(face)  # face_image: PIL.Image.Image
         img = img.resize(required_size)  # image: ndarray
         face_array = asarray(img)  # face_array: ndarray
@@ -96,6 +95,8 @@ def MTCNN_detect_face_2(filename, required_size = (160, 160)):
         return no_face_array
 
 
+    
+    
 """ Load image data to make X & y """
 
 # Load images and extract faces for all images in a directory
@@ -115,7 +116,7 @@ def load_dataset(dir):
     X = []
     y = []
     
-    # enumerate folders, on per class
+    # Enumerate folders on per class
     for subdir in listdir(dir):
         # path
         filepath = dir + subdir + '/'
@@ -133,6 +134,7 @@ def load_dataset(dir):
         
         X.extend(faces)
         y.extend(labels)
+        
     return asarray(X), asarray(y)
 
 
@@ -155,16 +157,16 @@ def load_dataset_2(dir):
 
     Imgs_DATA_DIR = []
     
-    # enumerate folders, on per class
+    # Enumerate folders on per class
     for subdir in listdir(dir):
         # path
         filepath = dir + subdir + '/'
         
-        # skip any files that might be in the dir
+        # Skip any files that might be in the dir
         if not isdir(filepath):
             continue
             
-        # load all faces in the subdirectory
+        # Load all faces in the subdirectory
         imgs_subdir = load_faces_2(filepath)
         
         
